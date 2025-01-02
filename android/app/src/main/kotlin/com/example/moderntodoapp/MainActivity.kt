8c614128a9c5cd5d8abea97672e8e8420b96052b
vortech <vortech@hxpredatorinterpolationhxphyper>
2025-01-02 21:31:32 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/moderntodoapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.moderntodoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:moderntodoapp/screens/homescreen.dart';
+
+void main() {
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
+      home: Home(),
+    );
+  }
+}
